5875a526aa6c48e0bd9a3e7b7d6f0dbed4619bc3
AlexToTheSun <alexsurf005@gmail.com>
2023-09-30 13:48:12 +0300

    Create Disk-Usage-Optimisation.sh

---
+++ Scripts/Disk-Usage-Optimisation.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+if [ ! $DIRECTORY ];
+  # if test ! $DIRECTORY ;
+  # if [ -z $DIRECTORY ]
+  # proverka peremennoy: https://g-soft.info/articles/7293/bash-proverit-pusta-li-peremennaya/
+  # proverka usliviy: https://www.opennet.ru/docs/RUS/bash_scripting_guide/c2171.html
+  then
+        read -p "Enter node directory starting: " DIRECTORY 
+          # about read: https://tokmakov.msk.ru/blog/item/68
+        echo 'export DIRECTORY='$DIRECTORY >> $HOME/.bash_profile
+        . ~/.bash_profile
+
+  #  elif [ ! $AGORIC_NODENAME ];
+fi
+
+source ~/.bash_profile
+echo 'Your node name: ' $DIRECTORY
+
+sed -i.bak -e "s/^indexer *=.*/indexer = \""null"\"/" $HOME/.$DIRECTORY/config/config.toml && \
+rm $HOME/.$DIRECTORY/data/tx_index.db/*
+echo DONE
