2cf69bf9967b904b3ee1c1dc03a5bf9aae39c48b
AlexToTheSun <alexsurf005@gmail.com>
2023-10-02 11:21:25 +0400

    Update server-protection.sh

--- Scripts/server-protection.sh
+++ Scripts/server-protection.sh
@@ -4,48 +4,58 @@
 # - Change the password,
 # - Change the SSH port,
 # - Firewall configuration (ufw)
-
+# case: https://www.opennet.ru/docs/RUS/bash_scripting_guide/x5210.html
+# case: https://routerus.com/bash-case-statement/
+# bash stsenari s operatorami if:  https://g-soft.info/articles/2814/usloviya-v-stsenariyah-bash-operatory-if/
+# Additionally:
+# otritsanie !: https://g-soft.info/articles/7293/bash-proverit-pusta-li-peremennaya/
+# https://www.opennet.ru/docs/RUS/bash_scripting_guide/c2171.html
 echo 'Module: Change the password'
 sleep 2
-read -p "Do you wish to Change the password? (y/n)" your_answer
+read -p "Do you wish to Change the password? (y/n): " your_answer
   case $your_answer in
-    [Yy]* ) passwd;
-    break;;
-    [Nn]* ) exit;;
+    [Yy]* ) passwd
+    ;;
+    # break;;
+    # error: ./server-protection.sh: line 13: break: only meaningful in a `for', `while', or `until' loop
+    [Nn]* )
+    ;;
   esac
 sleep 2
 
-echo "Change the SSH port"
+echo "Module: Change the SSH port"
 sleep 2
-read -p "Change the SSH port" your_answer
+read -p "Do you wish to Change the SSH port? (y/n): " your_answer
   case $your_answer in
-    [Yy]* ) read -p "Type your new SSH port" ssh_port;
+    [Yy]* ) read -p "Type your new SSH port: " ssh_port;
     echo 'export ssh_port='$ssh_port >> $HOME/.bash_profile
     . ~/.bash_profile
     sed -i.bak -e "s/^Port *=.*/Port = ssh_port/" /etc/ssh/sshd_config
     sudo ufw allow $ssh_port/tcp
     sudo ufw deny 22
-    break;;
-    [Nn]* ) exit;;
+    ;;
+    [Nn]* )
+    ;;
   esac
 sleep 2
 
 echo 'Module: Firewall configuration'
 sleep 2
-read -p "Do you wish to do Firewall' configuration? (y/n)" -n 3 your_answer
-  if [ "$your_answer" = "^[Yy]*" ];
+read -p "Do you wish to do Firewall' configuration? (y/n): " your_answer
+  if [[ "$your_answer" == [Yy]* ]]; 
+  #  don't work "^[Yy]*" with "" and ^
     then
     sudo apt update && sudo apt upgrde -y
     sudo apt install ufw -y
     sudo ufw allow ssh
-    read -p "Type your node' API server Port ( by default 1317)" api_port
-    read -p "Type your node' p2p connection Port ( by default 26656)" p2p_port
-    read -p "Type your node' RPC Port ( by default 26657)" rpc_port
-    read -p "Type your node' ABCI server Port ( by default 26658)" abci_port
-    read -p "Type your node' Prometheus  Port ( by default 26660)" prometheus_port
-    read -p "Type your node' pprof listen address Port ( by default 6060)" pprof_port
-    read -p "Type your node' gRPC Port ( by default 9090)" gRPC_port
-    read -p "Type your node' gRPC-web Port ( by default 9091)" gRPC_web_port
+    read -p "Type your node' API server Port ( by default 1317): " api_port
+    read -p "Type your node' p2p connection Port ( by default 26656): " p2p_port
+    read -p "Type your node' RPC Port ( by default 26657): " rpc_port
+    read -p "Type your node' ABCI server Port ( by default 26658): " abci_port
+    read -p "Type your node' Prometheus  Port ( by default 26660): " prometheus_port
+    read -p "Type your node' pprof listen address Port ( by default 6060): " pprof_port
+    read -p "Type your node' gRPC Port ( by default 9090): " gRPC_port
+    read -p "Type your node' gRPC-web Port ( by default 9091): " gRPC_web_port
     sudo ufw allow $ssh_port/tcp
     sudo ufw deny 22
     sudo ufw allow $api_port
@@ -61,12 +71,10 @@ read -p "Do you wish to do Firewall' configuration? (y/n)" -n 3 your_answer
     sudo ufw status
     sudo ufw status verbose
     ss -tulpn
-  elif [ "$your_answer" = "^[Nn]*" ];
+  elif [[ "$your_answer" == [Nn]* ]];
     then
     echo "Go to the next module"
   fi
-sleep 2
-done
 
 
 
