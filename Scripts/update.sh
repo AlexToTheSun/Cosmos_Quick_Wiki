757180bf08e78ffeffdc17cc11997f9c0db63a13
AlexToTheSun <alexsurf005@gmail.com>
2023-10-25 11:11:50 +0400

    some fixes update.sh

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 function send_telegram_message() {
-    local message="$1"
-    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_API_TOKEN/sendMessage" \
-        -d chat_id="$TELEGRAM_CHAT_ID" \
-        -d text="$message" >/dev/null
+  local message="$1"
+  curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_API_TOKEN/sendMessage" \
+    -d chat_id="$TELEGRAM_CHAT_ID" \
+    -d text="$message" 2>&1 1>/dev/null 
 }
 
 # Add variables
+. ~/.bash_profile
+if [ ! $TELEGRAM_API_TOKEN ]; then
+  read -p 'Your TELEGRAM API TOKEN. Example "826405938:djhNkduNekj4njmfjJJJDHkjdsJ732349kjdfD": ' TELEGRAM_API_TOKEN
+  echo 'export TELEGRAM_API_TOKEN='$TELEGRAM_API_TOKEN >> $HOME/.bash_profile
+  . ~/.bash_profile
+fi
+
+if [ ! $TELEGRAM_CHAT_ID  ]; then
+  read -p 'Your TELEGRAM CHAT ID. Example "947265947": ' TELEGRAM_CHAT_ID
+  echo 'export TELEGRAM_CHAT_ID='$TELEGRAM_CHAT_ID >> $HOME/.bash_profile
+  . ~/.bash_profile
+fi
+
 read -p 'Path to your current binary. Example "/usr/local/bin/haqqd": ' current_binary
 read -p 'Path to your new binary. Example "$HOME/go/bin/haqqd": ' new_binary
 read -p "Height of the last block, after that a chain will be stopped: " stop_height
@@ -29,17 +42,17 @@ fi
 # Upgrading of the binary file
 for ((;;))
 do
-	height=$(curl -s localhost:$rpc_port/status | jq -r .result.sync_info.latest_block_height)
-	if ! [ $height -eq $stop_height ]
-	then
-		echo $height
-	else
-		systemctl stop $service_name
-		mv $new_binary $current_binary
-		systemctl restart $service_name
-		echo 'Stopping, Changing the binary and Restarting done'
-		send_telegram_message "Stopping, Changing the binary and Restarting done. Please, check!"
-		breack
-	fi
-	sleep 4
+    height=$(curl -s localhost:$rpc_port/status | jq -r .result.sync_info.latest_block_height)
+    if ! [ $height -eq $stop_height ]
+    then
+      echo $height
+    else
+      systemctl stop $service_name
+      mv $new_binary $current_binary
+      systemctl restart $service_name
+      echo 'Stopping, Changing the binary and Restarting done'
+      send_telegram_message "!!!About ${service_name}: work is done. Stop, Change binary file, Restart. Please, check!"
+      breack
+    fi
+      sleep 4
 done
